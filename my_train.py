e779f8e13363f90ae4e413b21be3ef002115f138
Sam131112 <portkey1996@gmail.com>
2019-05-25 23:40:56 +0530

    Add files via upload

--- my_train.py
+++ my_train.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 from my_utils import get_data,accuracy
-from my_model import GCN
+from my_model import GCN,SimpleModel
 
 
 np.random.seed(42)
@@ -15,14 +15,16 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 adj,features,labels,idx_train,idx_val,idx_test = get_data(True)
 
 
-model = GCN(nfeat=features.shape[1],nhid =256,nclass=labels.max().item()+1,dropout=0.5)
+#model = GCN(nfeat=features.shape[1],nhid =256,nclass=labels.max().item()+1,dropout=0.5)
+model = SimpleModel(nfeat=features.shape[1],nhid=256,nclass=labels.max().item()+1)
 optimizer = optim.Adam(model.parameters(),lr = 0.01,weight_decay=5e-4)
 
 model.cuda()
 
 def train(epoch,x,adj,labels):
     optimizer.zero_grad()
-    output = model(x,adj)
+    #output = model(x,adj)
+    output = model(x)
     loss_train = F.nll_loss(output[idx_train],labels[idx_train])
     acc_train = accuracy(output[idx_train],labels[idx_train])
     loss_train.backward()
@@ -32,7 +34,8 @@ def train(epoch,x,adj,labels):
 
 
 def test(x,adj,labels):
-    output = model(x,adj)
+    #output = model(x,adj)
+    output = model(x)
     acc_test = accuracy(output[idx_test],labels[idx_test])
     print("Test set results:",acc_test.item())
 
